ba14b8dec44ccbcb5ceda970fd145abf2bdc9160
MaikonGithub <maikon.ferreirayt@gmail.com>
2022-09-09 14:28:26 -0300

    Fix folder organization

---
+++ Restart-App/Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Maikon Ferreira on 24/07/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
